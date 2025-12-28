b91e6bc0d874e5dc64fd25069c03beedae6144c2
Shraman Banerjee <shramanb1@gmail.com>
2025-12-29 01:40:22 +0530

    index.ts fixed

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,46 @@
-import  dotenv  from "dotenv";
-import express from "express";
-import authRouter from './router/auth.routes.ts' 
-import classRouter from "./router/class.routes.ts";
-import attendanceRouter from "./router/attendance.route.ts";
-dotenv.config()
+import express from 'express';
+import http from 'http';
+import dotenv from 'dotenv';
+import { connectDB } from './config/dbConfig.ts'; 
+import { setupWebSocketServer } from './socket/socket.ts'; 
+
+import authRouter from './router/auth.routes.ts';
+import classRouter from './router/class.routes.ts';
+import attendanceRouter from './router/attendance.route.ts';
+
+dotenv.config();
 
 const app = express();
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-
-app.use('/auth',authRouter)
-app.use("/class",classRouter)
-app.use("/attendance",attendanceRouter)
-
-
-app.get("/", (req, res) => {
-  res.json({ message: "Server running!" });
-});
-
-
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server started on http://localhost:${PORT}`);
+
+app.use(express.json());
+
+
+app.use('/auth', authRouter);
+app.use('/class', classRouter);
+app.use('/attendance', attendanceRouter);
+
+const server = http.createServer(app);
+
+
+setupWebSocketServer(server);
+
+connectDB()
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+      console.log(`WebSocket running on ws://localhost:${PORT}/ws`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to database:", error);
+    process.exit(1);
+  });
+
+app.use((err: any, req: any, res: any, next: any) => {
+  console.error("Unhandled Error:", err);
+  res.status(500).json({
+    success: false,
+    message: "Internal Server Error",
+  });
 });
